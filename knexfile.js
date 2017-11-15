3daf7898feabeb8a9156c9d6e117b3466ef5701f
COLE CARROLL <YOUR EMAIL ADDRESS>
2017-11-15 11:23:08 -0700

    setting up db

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,12 @@
+var env = require('dotenv').config()
+
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/insertTest'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
